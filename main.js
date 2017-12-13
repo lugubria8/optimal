0905fdd0bcc9835c829ab4e0b0b6b0369eff5147
Julia Mellen <juliamellen@Julias-MacBook-Pro-6.local>
2017-12-12 21:54:55 -0600

    oh god why isn't it changing, adding a split screen

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var cat = document.querySelector('#opt1');
-var cat = document.querySelector('#opt2');
+var cati = document.querySelector('#opt2');
 var catIndex = 0;
+var catiIndex = 0;
 
 function changeCat(){
 	var list = [
-  	'images/optipurp1.gif',
-		'images/optipurp2.gif',];
+		'images/optipurp1.gif',
+		'images/optipurp3.gif'];
   catIndex++;
   if (catIndex >= 1){
   	catIndex = 0;
@@ -14,4 +15,18 @@ function changeCat(){
 
 }
 
-cat.addEventListener( 'click', changeCat )
+function changeCati(){
+	var list = [
+		'images/optipurp2.gif',
+		'images/optipurp4.gif'];
+  catiIndex++;
+  if (catiIndex >= 1){
+  	catiIndex = 0;
+  }
+  cati.src = list[catiIndex];
+
+}
+
+
+cat.addEventListener( 'click', changeCat );
+// cati.addEventListener( 'click', changeCati );
